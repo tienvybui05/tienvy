44128af8096d161f263cb16d80adf5ba31f0fdda
ducnm2412 <ngominhduc11a9s@gmail.com>
2024-10-25 14:50:12 +0700

    add source
    Signed-off-by: ducnm2412 <ngominhduc11a9s@gmail.com>

---
+++ KoiFishServiceCenter.Repositories/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Repositories.Interfaces
+{
+    internal interface ICustomerRepository
+    {
+    }
+}
