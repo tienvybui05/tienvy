73a2caf02b54e9eeadd542fd8896275b6494fa46
DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>
2024-10-25 08:26:28 +0700

    Add interface and class of VetScheduleService

---
+++ KoiFishServiceCenter.Services/Services/VetScheduleService.cs
@@ -0,0 +1,49 @@
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using KoiFishServiceCenter.Repositories.Interfaces;
+using KoiFishServiceCenter.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Services.Services
+{
+    public class VetScheduleService : IVetScheduleService
+    {
+        private readonly IVetScheduleRepository _repository;
+        public VetScheduleService(IVetScheduleRepository repository)
+        {
+            _repository = repository;
+        }
+        public bool AddVetSchedule(VetSchedule vetSchedule)
+        {
+            return _repository.AddVetSchedule(vetSchedule);
+        }
+
+        public bool DelVetSchedule(int Id)
+        {
+            return _repository.DelVetSchedule(Id);
+        }
+
+        public bool DelVetSchedule(VetSchedule vetSchedule)
+        {
+            return _repository.DelVetSchedule(vetSchedule);
+        }
+
+        public Task<VetSchedule> GetVetScheduleById(int Id)
+        {
+            return _repository.GetVetScheduleById(Id);
+        }
+
+        public Task<List<VetSchedule>> GetVetSchedulesAsync()
+        {
+            return _repository.GetVetSchedulesAsync();
+        }
+
+        public bool UpdateVetSchedule(VetSchedule vetSchedule)
+        {
+           return _repository.UpdateVetSchedule(vetSchedule);
+        }
+    }
+}
--- KoiFishServiceCenter.Services/obj/Debug/net8.0/KoiFishServiceCenter.Services.AssemblyInfo.cs
+++ KoiFishServiceCenter.Services/obj/Debug/net8.0/KoiFishServiceCenter.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiFishServiceCenter.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e67386e34d4bad2ac34aa58fccf52789297e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiFishServiceCenter.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiFishServiceCenter.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
