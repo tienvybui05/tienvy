52a6b8ea65f5bf769e3fcd270e6ed9f6ceefe433
DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>
2024-10-25 10:44:07 +0700

    Update Interface VetScheduleService

---
+++ KoiFishServiceCenter.Services/Interfaces/IVetScheduleService.cs
@@ -0,0 +1,19 @@
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Services.Interfaces
+{
+    public interface IVetScheduleService
+    {
+       Task<List<VetSchedule>>GetVetSchedulesAsync();
+        bool DelVetSchedule(int Id);
+        bool DelVetSchedule(VetSchedule vetSchedule);
+        bool AddVetSchedule(VetSchedule vetSchedule);
+        bool UpdateVetSchedule(VetSchedule vetSchedule);
+        Task<VetSchedule> GetVetScheduleById(int Id);
+    }
+}
