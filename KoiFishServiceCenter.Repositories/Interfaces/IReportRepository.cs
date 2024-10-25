43739a796e0775ce21089461dd3b6f3d684c0b05
Huỳnh Tuấn Lượng <luongzz2105@gmail.com>
2024-10-25 17:39:08 +0700

    upload 2 file IReportRepository.cs & ReportRepository.cs
    Signed-off-by: Huỳnh Tuấn Lượng <tuanluongmct2005@gmail.com>

---
+++ KoiFishServiceCenter.Repositories/Interfaces/IReportRepository.cs
@@ -0,0 +1,19 @@
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Repositories.Interfaces
+{
+    public interface IReportRepository
+    {
+        Task<List<Report>> GetReportsAsync();
+        bool DelReport(int Id);
+        bool DelReport(Report report);
+        bool AddReport(Report report);
+        bool UpdateReport(Report report);
+        Task<Report> GetReportById(int Id);
+    }
+}
---
+++ KoiFishServiceCenter.Repositories/Repositories/ReportRepository.cs
@@ -0,0 +1,43 @@
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using KoiFishServiceCenter.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Repositories.Repositories
+{
+    public class ReportRepository : IReportRepository
+    {
+        public bool AddReport(Report report)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DelReport(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DelReport(Report report)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Report> GetReportById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Report>> GetReportsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateReport(Report report)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
