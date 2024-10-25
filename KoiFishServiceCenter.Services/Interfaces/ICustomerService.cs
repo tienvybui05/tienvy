5e30945f11176337796f6d93401f3e424defcf68
ducnm2412 <ngominhduc11a9s@gmail.com>
2024-10-25 16:55:22 +0700

    add ICustomerService add CustomerService

---
+++ KoiFishServiceCenter.Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiFishServiceCenter.Repositories;
+using KoiFishServiceCenter.Repositories.Entities;
+namespace KoiFishServiceCenter.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<List<Customer>> GetCustomerAsync();
+        bool DelCusomer(int Id);
+        bool DelCustomer(Customer customer);
+        bool AddCustomer(Customer customer);
+        bool UpdateCustomer(Customer customer);
+        Task<Customer> GetCustomer(int Id);
+    }
+}
---
+++ KoiFishServiceCenter.Services/Services/CustomerService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using KoiFishServiceCenter.Repositories.Entities;
+using KoiFishServiceCenter.Repositories.Interfaces;
+using KoiFishServiceCenter.Services.Interfaces;
+namespace KoiFishServiceCenter.Services.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _repsitory;
+        public CustomerService(ICustomerRepository repsitory)
+        {
+            _repsitory = repsitory;
+        }
+
+        public bool AddCustomer(Customer customer)
+        {
+           return _repsitory.AddCustomer(customer);
+        }
+
+        public bool DelCusomer(int Id)
+        {
+            return _repsitory.DelCusomer(Id);
+        }
+
+        public bool DelCustomer(Customer customer)
+        {
+            return _repsitory.DelCustomer(customer);
+        }
+
+        public Task<Customer> GetCustomer(int Id)
+        {
+            return _repsitory.GetCustomer(Id);
+        }
+
+        public Task<List<Customer>> GetCustomerAsync()
+        {
+            return _repsitory.GetCustomerAsync();
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            return _repsitory.UpdateCustomer(customer);
+        }
+    }
+}
