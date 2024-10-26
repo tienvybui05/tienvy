1371095d3d3448e239250c9a4244ab81700cfb4a
DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>
2024-10-26 15:12:24 +0700

    Add ServiceHistory
    Signed-off-by: DinhQuocDatIT <dinhquocdat2005thdgli@gmail.com>

---
+++ KoiFishServiceCenter.Services/Interfaces/IServiceHistoryService.cs
@@ -0,0 +1,19 @@
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Services.Interfaces
+{
+    public interface IServiceHistoryService
+    {
+        Task<List<ServiceHistory>> GetServiceHistories();
+        bool DelServiceHistory(int Id);
+        bool DelServiceHistory(ServiceHistory serviceHistory);
+        bool AddServiceHistory(ServiceHistory serviceHistory);
+        bool UpdateServiceHistory(ServiceHistory serviceHistory);
+        Task<ServiceHistory> GetServiceHistoryById(int Id);
+    }
+}
---
+++ KoiFishServiceCenter.Services/Services/ServiceHistoryService.cs
@@ -0,0 +1,49 @@
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using KoiFishServiceCenter.Repositories.Interfaces;
+using KoiFishServiceCenter.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishServiceCenter.Services.Services
+{
+    public class ServiceHistoryService : IServiceHistoryService
+    {
+        private readonly IServiceHistoryRepository _repository;
+        public ServiceHistoryService(IServiceHistoryRepository repository)
+        {
+            _repository = repository;
+        }
+        public bool AddServiceHistory(ServiceHistory serviceHistory)
+        {
+           return _repository.AddServiceHistory(serviceHistory);
+        }
+
+        public bool DelServiceHistory(int Id)
+        {
+            return _repository.DelServiceHistory(Id);
+        }
+
+        public bool DelServiceHistory(ServiceHistory serviceHistory)
+        {
+            return _repository.DelServiceHistory(serviceHistory);
+        }
+
+        public Task<List<ServiceHistory>> GetServiceHistories()
+        {
+           return _repository.GetServiceHistories();
+        }
+
+        public Task<ServiceHistory> GetServiceHistoryById(int Id)
+        {
+            return _repository.GetServiceHistoryById(Id);
+        }
+
+        public bool UpdateServiceHistory(ServiceHistory serviceHistory)
+        {
+            return _repository.UpdateServiceHistory(serviceHistory);
+        }
+    }
+}
