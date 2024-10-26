84088b1f1ea98a26effdc679733d63d9b44380a1
NhatDuy <duy2808abc@gmail.com>
2024-10-27 04:17:29 +0700

    Add ICostRepository.cs
    Signed-off-by: NhatDuy <duy2808abc@gmail.com>

---
+++ KoiFishServiceCenter.Repositories/Interfaces/ICostRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiFishServiceCenter.Repositories.Entities;
+
+namespace KoiFishServiceCenter.Repositories.Interfaces
+{
+    public interface ICostRepository
+    {
+        Task<List<Cost>> GetAllCostAsync();
+        Task<Cost> GetCostByIdAsync(int costId);
+        Task<bool> AddCostAsync(Cost cost);
+        Task<bool> DeleteCostByIdAsync(int costId);
+        Task<bool> DeleteCostAsync(Cost cost);
+        Task<bool> UpdateCostAsync(Cost cost);
+    }
+}
