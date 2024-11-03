342e381447468d0283a061aa4f9d59e6561552e4
Huỳnh Tuấn Lượng <luongzz2105@gmail.com>
2024-11-03 16:52:31 +0700

    Update services
    Signed-off-by: Huỳnh Tuấn Lượng <luongzz2105@gmail.com>

---
+++ KoiServiceCenter.WebApp/Pages/Admin/services/Edit.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KoiFishServiceCenter.Repositories.Entities;
+
+namespace KoiServiceCenter.WebApp.Pages.Admin.services
+{
+    public class EditModel : PageModel
+    {
+        private readonly KoiFishServiceCenter.Repositories.Entities.KoiVetServicesDbContext _context;
+
+        public EditModel(KoiFishServiceCenter.Repositories.Entities.KoiVetServicesDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Service Service { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Service = await _context.Services.FirstOrDefaultAsync(m => m.ServiceId == id);
+
+            if (Service == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Service).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServiceExists(Service.ServiceId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ServiceExists(int id)
+        {
+            return _context.Services.Any(e => e.ServiceId == id);
+        }
+    }
+}
