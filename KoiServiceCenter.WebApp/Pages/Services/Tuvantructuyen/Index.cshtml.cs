6f4356a97f9a5a2d2a141c5d9c7c04ebbe72f232
vocaotangoc <caongoctanvo@gmail.com>
2024-11-05 22:22:54 +0700

    add Services/Tuvantructuyen/Create.cshtml Services/Tuvantructuyen/Create.cshtml.cs add Services/Tuvantructuyen/Index.cshtml Services/Tuvantructuyen/Index.cshtml.cs update Services/Index.cshtml Signed-off-by: vocaotangoc <caongoctanvo@gmail.com>

---
+++ KoiServiceCenter.WebApp/Pages/Services/Tuvantructuyen/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KoiFishServiceCenter.Repositories.Entities;
+
+namespace KoiServiceCenter.WebApp.Pages.Services.Tuvantructuyen
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KoiFishServiceCenter.Repositories.Entities.KoiVetServicesDbContext _context;
+
+        public IndexModel(KoiFishServiceCenter.Repositories.Entities.KoiVetServicesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ServiceHistory> ServiceHistory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            ServiceHistory = await _context.ServiceHistories
+                .Include(s => s.Customer)
+                .Include(s => s.Service)
+                .Include(s => s.Veterinarian).ToListAsync();
+        }
+    }
+}
