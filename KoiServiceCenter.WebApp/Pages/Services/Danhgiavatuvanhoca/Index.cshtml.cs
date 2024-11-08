b991fa8b46b4ae6d3ad7bf5a86a8441a57033e7a
vocaotangoc <caongoctanvo@gmail.com>
2024-11-08 13:25:41 +0700

    update datlich
    Signed-off-by: vocaotangoc <caongoctanvo@gmail.com>

--- KoiServiceCenter.WebApp/Pages/Services/Danhgiavatuvanhoca/Index.cshtml.cs
+++ KoiServiceCenter.WebApp/Pages/Services/Danhgiavatuvanhoca/Index.cshtml.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KoiFishServiceCenter.Repositories.Entities;
+using KoiFishServiceCenter.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace KoiServiceCenter.WebApp.Pages.Services.Danhgiavatuvanhoca
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IServiceHistoryService _service;
+        private readonly IVetScheduleService _vetScheduleService;
+        public IndexModel(IServiceHistoryService service, IVetScheduleService vetScheduleService)
         {
+            _service = service;
+            _vetScheduleService = vetScheduleService;
+        }
+        //public CreateModel(IServiceHistoryService service)
+        //{
+        //    _service = service;
+        //}
+
+        public IActionResult OnGet()
+        {
+            ViewData["CustomerId"] = _service.GetServiceHistorySelect("CustomerId");
+            ViewData["ServiceId"] = _service.GetServiceHistorySelect("ServiceId");
+            ViewData["VeterinarianId"] = _service.GetServiceHistorySelect("VeterinarianId");
+            return Page();
+        }
+
+        [BindProperty]
+        public ServiceHistory ServiceHistory { get; set; }
+        public VetSchedule VetSchedule { get; set; }
+
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //await _service.AddServiceHistory(ServiceHistory);
+            var checkID = await _service.GetServiceHistoryById(ServiceHistory.HistoryId);
+            var checkDateTime = await _service.BundByDate(ServiceHistory);
+            if (checkID != null)
+            {
+                ModelState.AddModelError("ServiceHistory.HistoryId", "Mã đơn dịch vụ đã tồn tại. Vui lòng nhập mã khác.");
+                ViewData["CustomerId"] = _service.GetServiceHistorySelect("CustomerId");
+                ViewData["ServiceId"] = _service.GetServiceHistorySelect("ServiceId");
+                ViewData["VeterinarianId"] = _service.GetServiceHistorySelect("VeterinarianId");
+                return Page();
+            }
+            if (checkDateTime == false)
+            {
+                ModelState.AddModelError("ServiceHistory.ServiceDate", "Bác sĩ đã có lịch. Vui lòng chọn ngày khác.");
+                ViewData["CustomerId"] = _service.GetServiceHistorySelect("CustomerId");
+                ViewData["ServiceId"] = _service.GetServiceHistorySelect("ServiceId");
+                ViewData["VeterinarianId"] = _service.GetServiceHistorySelect("VeterinarianId");
+                return Page();
+            }
+            else
+            {
+                await _service.AddServiceHistory(ServiceHistory);
+                bool check = false;
+                Random random = new Random();
+                int ranDumID;
+                do
+                {
+                    ranDumID = random.Next(1, 1001);
+                    var x = await _service.GetServiceHistoryById(ranDumID);
+                    if (x == null)
+                    {
+                        check = true;
+                    }
+
+                } while (check != true);
+
+                VetSchedule = new VetSchedule();
+                VetSchedule.ScheduleId = ranDumID;
+                VetSchedule.VeterinarianId = ServiceHistory.VeterinarianId;
+                VetSchedule.ScheduleDate = ServiceHistory.ServiceDate;
+                ViewData["VeterinarianId"] = _vetScheduleService.GetVeterinarianSelect();
+                await _vetScheduleService.AddVetSchedule(VetSchedule);// Thêm vào lịch làm việc của bác sĩ
+
+
+                return RedirectToPage("./Index");
+            }
         }
     }
 }
