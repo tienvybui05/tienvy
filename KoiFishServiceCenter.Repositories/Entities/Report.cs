897667421640c04e851bc130154e51e359d2878c
Huỳnh Tuấn Lượng <luongzz2105@gmail.com>
2024-11-13 00:49:01 +0700

    Update Report Id
    Signed-off-by: Huỳnh Tuấn Lượng <luongzz2105@gmail.com>

--- KoiFishServiceCenter.Repositories/Entities/Report.cs
+++ KoiFishServiceCenter.Repositories/Entities/Report.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace KoiFishServiceCenter.Repositories.Entities;
 
 public partial class Report
 {
+    [Required(ErrorMessage = "{0} Vui lòng nhập")]
+    [DisplayName("Mã báo cáo")]
     public int ReportId { get; set; }
-
+    [DisplayName("Ngày làm")]
     public DateTime ReportDate { get; set; }
-
+    [DisplayName("Tổng khách hàng")]
     public int? TotalCustomers { get; set; }
-
+    [DisplayName("Tổng dịch vụ")]
     public int? TotalServices { get; set; }
-
+    [DisplayName("Điểm đánh giá")]
     public decimal? AverageRating { get; set; }
-
+    [DisplayName("Ghi chú")]
     public string? Notes { get; set; }
 }
