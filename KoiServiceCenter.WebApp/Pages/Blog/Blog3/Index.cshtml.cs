f2175bbf271ddb08586609f3e1ede7560550bf05
NhatDuy <duy2808abc@gmail.com>
2024-11-13 06:04:30 +0700

    add Blog
    Signed-off-by: NhatDuy <duy2808abc@gmail.com>

---
+++ KoiServiceCenter.WebApp/Pages/Blog/Blog3/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiServiceCenter.WebApp.Pages.Blog.Blog3
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KoiServiceCenter.WebApp/Pages/Blog/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiServiceCenter.WebApp.Pages.Blog
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
