8a6c3f478bb6ec96ae603bb51d4085a04346f4a5
NhatDuy <duy2808abc@gmail.com>
2024-11-15 04:21:43 +0700

    add Book, update Pages

---
+++ KoiServiceCenter.WebApp/Pages/Services/Book/Index.cshtml.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KoiFishServiceCenter.Repositories.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using KoiFishServiceCenter.Services.Interfaces;
+using KoiFishServiceCenter.Services.Services;
+
+namespace KoiServiceCenter.WebApp.Pages.Services.Book
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IServiceHistoryService _service;
+        private readonly IVetScheduleService _vetScheduleService;
+        public IndexModel(IServiceHistoryService service, IVetScheduleService vetScheduleService)
+        {
+            _service = service;
+            _vetScheduleService = vetScheduleService;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            int ranDumID;
+            ranDumID = await _service.CreateId();
+
+            ServiceHistory = new ServiceHistory
+            {
+                HistoryId = ranDumID
+            };
+            // Load các ViewData khác n?u c?n
+            ViewData["CustomerId"] = _service.GetServiceHistorySelect("CustomerId");
+            ViewData["ServiceId"] = _service.GetServiceHistorySelect("ServiceId");
+            ViewData["VeterinarianId"] = _service.GetServiceHistorySelect("VeterinarianId");
+
+            return Page();
+        }
+
+
+        [BindProperty]
+        public ServiceHistory ServiceHistory { get; set; }
+        public VetSchedule VetSchedule { get; set; }
+
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+
+            //await _service.AddServiceHistory(ServiceHistory);
+            var checkID = await _service.GetServiceHistoryById(ServiceHistory.HistoryId);
+            var checkDateTime = await _service.BundByDate(ServiceHistory);
+            if (checkID != null)
+            {
+                ModelState.AddModelError("ServiceHistory.HistoryId", "Mã ??n d?ch v? ?ã t?n t?i. Vui lòng nh?p mã khác.");
+                ViewData["CustomerId"] = _service.GetServiceHistorySelect("CustomerId");
+                ViewData["ServiceId"] = _service.GetServiceHistorySelect("ServiceId");
+                ViewData["VeterinarianId"] = _service.GetServiceHistorySelect("VeterinarianId");
+                return Page();
+            }
+            if (checkDateTime == false)
+            {
+                ModelState.AddModelError("ServiceHistory.ServiceDate", "Bác s? ?ã có l?ch. Vui lòng ch?n ngày khác.");
+                ViewData["CustomerId"] = _service.GetServiceHistorySelect("CustomerId");
+                ViewData["ServiceId"] = _service.GetServiceHistorySelect("ServiceId");
+                ViewData["VeterinarianId"] = _service.GetServiceHistorySelect("VeterinarianId");
+                return Page();
+            }
+            else
+            {
+                await _service.AddServiceHistory(ServiceHistory);
+                bool check = false;
+                Random random = new Random();
+                int ranDumID;
+                do
+                {
+                    ranDumID = random.Next(1, 1001);
+                    var x = await _service.GetServiceHistoryById(ranDumID);
+                    if (x == null)
+                    {
+                        check = true;
+                    }
+
+                } while (check != true);
+                VetSchedule = new VetSchedule();
+                VetSchedule.ScheduleId = ranDumID;
+                VetSchedule.VeterinarianId = ServiceHistory.VeterinarianId;
+                VetSchedule.ScheduleDate = ServiceHistory.ServiceDate;
+                ViewData["VeterinarianId"] = _vetScheduleService.GetVeterinarianSelect();
+                await _vetScheduleService.AddVetSchedule(VetSchedule);// Thêm vào l?ch làm vi?c c?a bác s?
+
+                return Redirect(Url.Page("/Services/Datlichthanhcong/Index"));
+            }
+        }
+    }
+}
