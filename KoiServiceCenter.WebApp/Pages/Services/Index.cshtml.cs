44a30f4018a08430ded67bd6229395fa652375d2
vocaotangoc <caongoctanvo@gmail.com>
2024-11-16 09:46:52 +0700

    update danhgia
    Signed-off-by: vocaotangoc <caongoctanvo@gmail.com>

--- KoiServiceCenter.WebApp/Pages/Services/Index.cshtml.cs
+++ KoiServiceCenter.WebApp/Pages/Services/Index.cshtml.cs
@@ -1,12 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KoiFishServiceCenter.Repositories.Entities;
+using KoiFishServiceCenter.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace KoiServiceCenter.WebApp.Pages.Services
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IFeedbackService _service;
+
+        public IndexModel(IFeedbackService service)
         {
+            _service = service;
+        }
+
+        public IList<Feedback> Feedback { get; set; }
+
+        public async Task OnGetAsync(string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                Feedback = await _service.SearchAsync(searchString);
+            }
+            else
+            {
+                //Nếu không có FeedbackId, lấy tất cả feedbacks
+                Feedback = await _service.GetFeedbacksAsync();
+            }
         }
     }
 }
